a66deef52a12dfd3e94ce8a9d91abd8401ea1ceb
Nicolae Aurelian <nicolaeaurelian-31@gmail.com>
2023-04-30 18:56:30 +0300

    fifth commit

--- java.js
+++ java.js
@@ -1,42 +1,33 @@
 function generateButtons() {
     const numButtons = document.getElementById("numButtons").value;
     const buttonContainer = document.getElementById("buttonContainer");
-    
-    // We save a random number between 1 and numButtons in a variable
+
     const winningButtonIndex = Math.floor(Math.random() * numButtons) + 1;
 
-    // Generate new buttons
     for (let i = 1; i <= numButtons; i++) {
         const newButton = document.createElement("button");
         newButton.innerText = "Button " + i;
         newButton.classList.add("button");
         buttonContainer.appendChild(newButton);
-       
-        // Set the status of the buttons (winner or loser) using the variable winningButtonIndex
-        if (i === winningButtonIndex) {
-            newButton.dataset.status = "winner";
-          } else {
-            newButton.dataset.status = "loser";
-          }
 
-           // Define a function that at click of the new button will show its status
-           newButton.addEventListener('click', handleButtonClick);
-           
+        const status = (i === winningButtonIndex) ? "winner" : "loser";
+        newButton.addEventListener('click', createButtonClickHandler(status));
     }
 
-    // We get rid of the input box and the generator button
     document.getElementById("numButtons").style.display = "none";
     document.getElementById("buttonId").style.display = "none";
 }
 
-function handleButtonClick() {
-    const buttonStatus = this.dataset.status;
-    let message;
-    if (buttonStatus === "winner") {
-        message = "Winner!";
-    } else {
-        message = "Loser!";
-    }
-    messageContainer.innerText = message;
+function createButtonClickHandler(status) {
+    return function() {
+        let message;
+        if (status === "winner") {
+            message = "Winner!";
+        } else {
+            message = "Loser!";
+        }
+        messageContainer.innerText = message;
+    };
 }
+
 
